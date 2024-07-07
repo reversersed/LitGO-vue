4c8f55fb1b0af3b806eb9a60b2a7241e3f286f74
hedgehog <myregisg@gmail.com>
2024-07-07 10:11:47 +0300

    Added footer ? Background color ? Text color

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import "../assets/variables.css";
+</script>
+
+<template>
+	<footer>
+		<p>LitGO © 2024</p>
+	</footer>
+</template>
+
+<style scoped>
+footer {
+	background-color: var(--accent-color);
+	width: 100%;
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	color: var(--main-text-color);
+}
+</style>
