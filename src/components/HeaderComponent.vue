b2dd558e5cfd093f25d5050fbda011a146f74d47
hedgehog <myregisg@gmail.com>
2024-07-07 09:50:16 +0300

    Removed dark background on searching Added <a> to header title

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,96 +3,87 @@ import "../assets/variables.css";
 import SearchFieldComponent from "../components/SearchFieldComponent.vue";
 </script>
 
-
 <template>
-    <header>
-        <div class="logo-wrapper">
-            <h1>LitGO</h1>
-        </div>
+	<header>
+		<div class="logo-wrapper">
+			<h1><a class="silent-a" href="/">LitGO</a></h1>
+		</div>
 
-        <div class="catalog-wrapper">
-            <button class="btn">
-                Каталог
-            </button>
-            <div class="show-catalog">
-
-            </div>
-        </div>
-        <div class="search-field-wrapper">
-            <SearchFieldComponent />
-        </div>
-        <div class="buttons">
-            <div class="icon-btn">
-                Отложенные
-            </div>
-            <div class="icon-btn">
-                Корзина
-            </div>
-            <div class="icon-btn">
-                Мои книги
-            </div>
-            <div class="icon-btn">
-                Войти
-            </div>
-        </div>
-    </header>
+		<div class="catalog-wrapper">
+			<button class="btn">Каталог</button>
+			<div class="show-catalog"></div>
+		</div>
+		<div class="search-field-wrapper">
+			<SearchFieldComponent />
+		</div>
+		<div class="buttons">
+			<div class="icon-btn">Отложенные</div>
+			<div class="icon-btn">Корзина</div>
+			<div class="icon-btn">Мои книги</div>
+			<div class="icon-btn">Войти</div>
+		</div>
+	</header>
 </template>
 
 <style scoped>
+.silent-a {
+	text-decoration: none;
+	color: var(--main-text-color);
+}
 .icon-btn {
-    color: var(--main-text-color);
-    font-family: var(--main-font-style);
-    cursor: pointer;
+	color: var(--main-text-color);
+	font-family: var(--main-font-style);
+	cursor: pointer;
 }
 
 .icon-btn:hover {
-    color: var(--accent-color);
+	color: var(--accent-color);
 }
 
 header {
-    display: flex;
-    align-items: center;
-    padding: 0px 10%;
+	display: flex;
+	align-items: center;
+	padding: 0px 10%;
 }
 
 h1 {
-    color: #FEFFFF;
-    margin: 0px;
-    font-family: var(--main-font-style);
+	color: #feffff;
+	margin: 0px;
+	font-family: var(--main-font-style);
 }
 
 header {
-    height: 64px;
-    background-color: #3AAFA9;
+	height: 64px;
+	background-color: #3aafa9;
 }
 
 .search-field-wrapper {
-    flex: 1;
-    padding: 0px 5%;
+	flex: 1;
+	padding: 0px 5%;
 }
 
 .catalog-wrapper button {
-    border: 0px;
-    background-color: #17252A;
-    padding: 10px 25px;
-    border-radius: 10px;
-    cursor: pointer;
-    margin: 10px;
-    color: #FEFFFF;
-    font-size: 14px;
-    font-weight: 600;
-    font-family: var(--main-font-style);
+	border: 0px;
+	background-color: #17252a;
+	padding: 10px 25px;
+	border-radius: 10px;
+	cursor: pointer;
+	margin: 10px;
+	color: #feffff;
+	font-size: 14px;
+	font-weight: 600;
+	font-family: var(--main-font-style);
 }
 
 .catalog-wrapper button:hover {
-    background-color: var(--shadow-background-color);
+	background-color: var(--shadow-background-color);
 }
 
 header .buttons {
-    display: flex;
+	display: flex;
 }
 
 header .buttons div {
-    margin: 10px;
+	margin: 10px;
 }
 </style>
--- src/components/SearchFieldComponent.vue
+++ src/components/SearchFieldComponent.vue
@@ -1,86 +1,66 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 import "../assets/variables.css";
-
-const state = ref<{
-    showBackground: boolean,
-}>({
-    showBackground: false
-});
-
-const onFocus = () => {
-    state.value.showBackground = true;
-}
-
 </script>
 
-
 <template>
-    <div class="fool-background" v-if="state.showBackground" @click="state.showBackground = false">
-
-    </div>
-    <div class="search-wrapper">
-        <input placeholder="Искать на LitGO" type="text" name="" id="" v-on:focus="onFocus">
-        <button>Найти</button>
-    </div>
+	<div class="search-wrapper">
+		<input
+			placeholder="Искать на LitGO"
+			type="text"
+			name=""
+			id=""
+		/>
+		<button>Найти</button>
+	</div>
 </template>
 
 <style scoped>
-.fool-background {
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    top: 0px;
-    left: 0px;
-    background-color: rgba(0, 0, 0, 0.253);
-    z-index: 1;
-}
-
 .search-wrapper {
-    z-index: 10000000;
-    background-color: #ffffff;
-    border: 2px solid var(--shadow-background-color);
-    border-radius: 10px;
-    display: flex;
-    align-items: center;
-    
+	z-index: 10000000;
+	background-color: #ffffff;
+	border: 2px solid var(--shadow-background-color);
+	border-radius: 10px;
+	display: flex;
+	align-items: center;
 }
 
 .search-wrapper input {
-    z-index: 10000000;
-    border: 0px;
-    outline: hidden;
-    outline: transparent;
-    padding: 10px;
-    border-radius: 10px;
-    min-width: 300px;
-    flex: 1;
-    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-weight: 700;
-    font-size: 16px;
-    color: var(--accent-color);
-    line-height: 22px;
-    -webkit-text-size-adjust: 100%;
-    -webkit-font-smoothing: antialiased;
+	z-index: 10000000;
+	border: 0px;
+	outline: hidden;
+	outline: transparent;
+	padding: 10px;
+	border-radius: 10px;
+	min-width: 300px;
+	flex: 1;
+	font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+		Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+	font-weight: 700;
+	font-size: 16px;
+	color: var(--accent-color);
+	line-height: 22px;
+	-webkit-text-size-adjust: 100%;
+	-webkit-font-smoothing: antialiased;
 }
 
 button {
-    z-index: 10000000;
-    height: 100%;
-    width: 100px;
-    border: 0px;
-    background-color: var(--shadow-background-color);
-    color: var(--main-text-color);
-    padding: 14px;
-    border-radius: 5px;
-    border-top-left-radius: 10px;
-    border-bottom-left-radius: 10px;
-    cursor: pointer;
+	z-index: 10000000;
+	height: 100%;
+	width: 100px;
+	border: 0px;
+	background-color: var(--shadow-background-color);
+	color: var(--main-text-color);
+	padding: 14px;
+	border-radius: 5px;
+	border-top-left-radius: 10px;
+	border-bottom-left-radius: 10px;
+	cursor: pointer;
 }
 
 button:hover {
-    z-index: 10000000;
-    background-color: var(--main-background-color);
-    border-radius: 7px;
+	z-index: 10000000;
+	background-color: var(--main-background-color);
+	border-radius: 7px;
 }
 </style>
