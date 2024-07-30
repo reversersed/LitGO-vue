142a2ac67c3568c323c1b5d29dfd5d80c6df5dd5
hedgehog <myregisg@gmail.com>
2024-07-30 13:05:10 +0300

    add comments

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-
-
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "home",
-            component: () => import("@/pages/MainPage.vue")
-        },
-        { 
-            path: '/:pathMatch(.*)*',
-            redirect: "/"
-        },
-    ]
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: () => import("@/pages/MainPage.vue"),
+		},
+		{
+			path: "/:pathMatch(.*)*",
+			redirect: "/", // 404 redirecting to index
+		},
+	],
 });
 
-export default router
+export default router;
