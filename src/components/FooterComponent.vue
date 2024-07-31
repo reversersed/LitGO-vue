10f7e771e88801164dcd105b17de8dfa73ed0a58
reversersed <myregisg@gmail.com>
2024-07-31 22:56:09 +0300

    make footer stay at bottom of page

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,7 +2,7 @@
 
 <template>
 	<footer
-		class="w-full relative flex flex-col items-center text-black bg-accent-light p-3"
+		class="w-full sticky top-[100vh] flex flex-col items-center text-black bg-accent-light p-6"
 	>
 		<p>LitGO © 2024</p>
 	</footer>
