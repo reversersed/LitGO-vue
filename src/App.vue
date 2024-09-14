34cce0f8ac58e31631989ebe3ab77fd1601079b9
reversersed <myregisg@gmail.com>
2024-09-14 19:56:53 +0300

    added dockerization for frontend

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import UserHttpService from "@/service/HttpService/userHttpService";
-import { OnUserLogin } from "@/service/plugins/userStatePlugin";
 import AppLayout from "./layouts/AppLayout.vue";
 </script>
 
-<script lang="ts">
-await new UserHttpService()
-	.checkForAuthorization()
-	.then((response) => response !== undefined && OnUserLogin(response));
-</script>
-
 <template>
-	<AppLayout>
-		<RouterView />
-	</AppLayout>
+	<Suspense>
+		<AppLayout>
+			<RouterView />
+		</AppLayout>
+	</Suspense>
 </template>
