6d0828b49b1e962b6a3b1691622ff7b44bf037d0
reversersed <myregisg@gmail.com>
2024-09-18 22:14:21 +0300

    change book and genre model

--- src/components/GenreObserverComponent.vue
+++ src/components/GenreObserverComponent.vue
@@ -17,7 +17,7 @@ const categories = await service.getAll();
 		>
 		<div v-for="category in categories" class="mb-2 mt-4 flex flex-col">
 			<a
-				:href="'/genre/' + category.translitName"
+				:href="'/genre/' + category.translitname"
 				class="hover:text-contrast transition-colors duration-200 w-fit cursor-pointer font-semibold font-main text-lg md:text-xl"
 				>{{ category.name }}
 				<FontAwesomeIcon
@@ -27,12 +27,12 @@ const categories = await service.getAll();
 			/></a>
 			<div class="flex flex-row flex-wrap break-words w-full h-auto mt-2">
 				<a
-					:href="'/genre/' + genre.translitName"
+					:href="'/genre/' + genre.translitname"
 					v-for="genre in category.genres"
 					class="cursor-pointer mr-4 md:mr-8 mb-1 md:mb-2 text-sm md:text-base sm:font-semibold text-contrast hover:text-contrast/60"
 					><span class="transition-all duration-200">{{ genre.name }}</span
 					><span class="font-main !text-mainblack/60 ml-1">{{
-						genre.bookCount ?? ""
+						genre.bookcount ?? ""
 					}}</span></a
 				>
 			</div>
--- src/models/genre.models.ts
+++ src/models/genre.models.ts
@@ -1,11 +1,11 @@
 export default class Genre {
-	public translitName: string;
+	public translitname: string;
 	public name: string;
-	public bookCount: number;
+	public bookcount: number;
 
 	constructor(_translitName: string, _name: string, _bookCount: number) {
-		this.translitName = _translitName;
+		this.translitname = _translitName;
 		this.name = _name;
-		this.bookCount = _bookCount;
+		this.bookcount = _bookCount;
 	}
 }
