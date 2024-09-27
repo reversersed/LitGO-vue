631d6d2c72f172b309e0939fa46b41a85a9ac4d2
reversersed <myregisg@gmail.com>
2024-09-27 20:52:31 +0300

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server:{
-    port: 7000
-  }
-})
+	plugins: [
+		vue(),
+		vueJsx(),
+		VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
+	],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	server: {
+		port: 7000,
+	},
+});
